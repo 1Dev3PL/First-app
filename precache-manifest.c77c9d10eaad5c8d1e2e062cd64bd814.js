2e34481ecb55582100cef7c9addb34ba05752389
1Dev3PL <Ivan3nd3r11@gmail.com>
2019-09-29 13:46:09 +0300

    Updates

---
+++ precache-manifest.c77c9d10eaad5c8d1e2e062cd64bd814.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e0dfc6066ddfc38c40ab7b6b832f624",
+    "url": "/First-app/index.html"
+  },
+  {
+    "revision": "ef430b6d0137fa07100e",
+    "url": "/First-app/static/css/3.4cf0ed20.chunk.css"
+  },
+  {
+    "revision": "bf1bb88e97a33ef0e577",
+    "url": "/First-app/static/css/4.1ab00573.chunk.css"
+  },
+  {
+    "revision": "741c4bbd1c29c2013a47",
+    "url": "/First-app/static/css/main.e8da4837.chunk.css"
+  },
+  {
+    "revision": "0d8b3602daf8999b4237",
+    "url": "/First-app/static/js/2.7b76bc86.chunk.js"
+  },
+  {
+    "revision": "ef430b6d0137fa07100e",
+    "url": "/First-app/static/js/3.dbf394a8.chunk.js"
+  },
+  {
+    "revision": "bf1bb88e97a33ef0e577",
+    "url": "/First-app/static/js/4.657aa2a5.chunk.js"
+  },
+  {
+    "revision": "741c4bbd1c29c2013a47",
+    "url": "/First-app/static/js/main.2b031626.chunk.js"
+  },
+  {
+    "revision": "431ab953f6d1ffbfe923",
+    "url": "/First-app/static/js/runtime~main.a859fd30.js"
+  }
+]);
